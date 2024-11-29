4a92901bcb0d1fa720183d4eb68ae5be954c3082
adarsh716 <alakhanpal2003@gmail.com>
2024-11-29 16:30:17 +0530

    change to white

--- src/components/core/HomePage/Timeline.jsx
+++ src/components/core/HomePage/Timeline.jsx
@@ -56,19 +56,19 @@ const TimelineSection = () => {
           })}
         </div>
         <div className="relative w-fit h-fit shadow-blue-200 shadow-[0px_0px_30px_0px]">
-          <div className="absolute lg:left-[50%] lg:bottom-0 lg:translate-x-[-50%] lg:translate-y-[50%] bg-caribbeangreen-700 flex lg:flex-row flex-col text-white uppercase py-5 gap-4 lg:gap-0 lg:py-10 ">
+          <div className="absolute lg:left-[50%] lg:bottom-0 lg:translate-x-[-50%] lg:translate-y-[50%] bg-richblue-500 flex lg:flex-row flex-col text-white uppercase py-5 gap-4 lg:gap-0 lg:py-10 ">
             {/* Section 1 */}
-            <div className="flex gap-5 items-center lg:border-r border-caribbeangreen-300 px-7 lg:px-14">
+            <div className="flex gap-5 items-center lg:border-r border-blue-25 text-blue-25 px-7 lg:px-14">
               <h1 className="text-3xl font-bold w-[75px]">10</h1>
-              <h1 className="text-caribbeangreen-300 text-sm w-[75px]">
+              <h1 className="text-blue-25 text-sm w-[75px]">
                 Years experiences
               </h1>
             </div>
 
             {/* Section 2 */}
             <div className="flex gap-5 items-center lg:px-14 px-7">
-              <h1 className="text-3xl font-bold w-[75px]">250</h1>
-              <h1 className="text-caribbeangreen-300 text-sm w-[75px]">
+              <h1 className="text-3xl text-blue-25 font-bold w-[75px]">250</h1>
+              <h1 className="text-blue-25 text-sm w-[75px]">
                 types of courses
               </h1>
             </div>
