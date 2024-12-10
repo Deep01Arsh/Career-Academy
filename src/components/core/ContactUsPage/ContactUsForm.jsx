f5cf4a3e2bcd9eaa63906c0a13d311f8475086cb
Deep01Arsh <iarshdeeps77@gmail.com>
2024-12-10 21:04:55 +0530

    done

--- src/components/core/ContactUsPage/ContactUsForm.jsx
+++ src/components/core/ContactUsPage/ContactUsForm.jsx
@@ -151,7 +151,7 @@ const ContactUsForm = () => {
           id="message"
           rows="6"
           placeholder="Write your message here..."
-          className="bg-white border border-blue-300 text-black px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+          className="bg-white border border-blue-300  text-black px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
           {...register("message", { required: true })}
         />
         {errors.message && (
